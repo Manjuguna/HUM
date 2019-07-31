53e73d4f78218e56908db4976ac7c36749108407
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-07-31 15:27:48 +0530

    Create lose.py

---
+++ lose.py
@@ -0,0 +1,11 @@
+nt,mt=list(map(int,input().split()))
+lit=list(map(int,input().split()))
+lit.sort(reverse=True)
+cat=0
+for i in lit:
+    if mt==0:
+        break
+    qr=mt // i
+    cat+=qr
+    mmmt=mt-i*qr
+print(cat)
